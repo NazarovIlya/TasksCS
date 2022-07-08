6e30407851d3da70b951a232b77ac98ae51f395f
NazarovIlya <eliakohai@gmail.com>
2022-07-09 00:35:12 +0300

    maxumu around 3

---
+++ Task_004_maxOf3/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 4: Напишите программу, которая принимает на
+вход три числа и выдаёт максимальное из этих чисел.*/
+
+Console.WriteLine("Введите целое число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите целое число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите целое число 3: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+if(num1 > max)
+{
+    max = num1;
+}
+if(num2 > max)
+{
+    max = num2;
+}
+if(num3 > max)
+{
+    max = num3;
+}
+
+Console.WriteLine($"Максимальное число = {max}");
