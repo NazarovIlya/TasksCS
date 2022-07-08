2cf47407b4a0c2ccb95283c8f8ece219ba9b8bea
NazarovIlya <eliakohai@gmail.com>
2022-07-08 23:54:47 +0300

    min_max

---
+++ Task_002_max_min/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 2: Напишите программу, которая на вход
+принимает два числа и выдаёт, какое число большее, а
+какое меньшее.*/
+Console.Clear();
+Console.WriteLine("Введите первое целое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+int min = 0;
+if(num1 == num2)
+{
+    Console.WriteLine($"Введеные числа равны {num1} = {num2}");
+} else
+if(num1 > num2)         // использован оператор if, так как на вход поступает только два числа
+{
+    Console.WriteLine($"Максимальное число = {num1}");  // в случае с большим количеством чисел
+    Console.WriteLine($"Минимальное число = {num2}");   // были бы использованы доп. переменые min и max
+} else
+{
+    Console.WriteLine($"Максимальное число = {num2}");
+    Console.WriteLine($"Минимальное число = {num1}");
+}
