52d8037483ce8076671385f519020a837c92a405
NazarovIlya <eliakohai@gmail.com>
2022-07-09 00:57:34 +0300

    even

--- Task_006_even/Program.cs
+++ Task_006_even/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 6: Напишите программу, которая на вход
+принимает число и выдаёт, является ли число чётным
+(делится ли оно на два без остатка).*/
+
+Console.WriteLine("Введите целое натуральное число: ");
+int bigNumber = Convert.ToInt32(Console.ReadLine());
+
+int even = 2;
+while(even <= bigNumber)
+{
+    Console.Write($"{even} ");
+    even = even + 2;
+}
