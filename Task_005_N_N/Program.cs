c97721d5294662f1b3b20c314d261e8818b20bc8
NazarovIlya <eliakohai@gmail.com>
2022-07-10 07:10:56 +0300

    interval from -N to N

---
+++ Task_005_N_N/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, которая на вход принимает
+одно число (N), а на выходе показывает все целые
+числа в промежутке от -N до N.*/
+
+Console.Clear();
+Console.WriteLine("Введите любое натуральное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int i;
+i = - N;
+while(i <= N)
+{
+    Console.Write($"{i} ");
+    i++;
+}
