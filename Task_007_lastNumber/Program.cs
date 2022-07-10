de5efc4031bd088a6492a5714e8843a878b65a34
NazarovIlya <eliakohai@gmail.com>
2022-07-10 07:23:46 +0300

    last number (345 -> 5)

--- Task_007_lastNumber/Program.cs
+++ Task_007_lastNumber/Program.cs
@@ -2,7 +2,8 @@
 трёхзначное число и на выходе показывает последнюю цифру
 этого числа.*/
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите целое трехзначное цисло: ");
 int num = Convert.ToInt32(Console.ReadLine());
-
-
+int lastNumber = 0;
+lastNumber = num % 10;
+Console.WriteLine($"Последняя цифра данного трехзначного числа {lastNumber}");
