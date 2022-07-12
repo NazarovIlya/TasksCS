76622d75c21a32336b5d2b88190d996269f78171
NazarovIlya <eliakohai@gmail.com>
2022-07-13 00:11:47 +0300

    Remove Second digital

---
+++ Task_010_RemoveSecond/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int num = new Random().Next(100, 1000);
+
+int RemoveSecond(int num)
+{
+    int first = num / 100;
+    int third = num % 10;
+    return first * 10 + third;
+}
+int result = RemoveSecond(num);
+Console.WriteLine($"{num} --> {result}");
+
