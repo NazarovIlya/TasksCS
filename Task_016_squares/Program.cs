67c369a550ee3e057f59614feb9d373d656b1eae
NazarovIlya <eliakohai@gmail.com>
2022-07-12 23:09:11 +0300

    squares

---
+++ Task_016_squares/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на
+// вход два числа и проверяет, является ли одно
+// число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+bool IsSquare(int a, int b)
+{
+    return a * a == b || b * b == a;
+}
+
+Console.WriteLine("Введите первое целое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool result = IsSquare(num1, num2);
+
+if (result)
+{
+    Console.WriteLine($"{num1}, {num2} -> yes");
+}
+else
+{
+    Console.WriteLine($"{num1}, {num2} -> no");
+}
