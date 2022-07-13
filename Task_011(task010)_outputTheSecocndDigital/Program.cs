495a34065df7e2dffeb65df89a2ce88449bf15cb
NazarovIlya <eliakohai@gmail.com>
2022-07-13 09:52:30 +0300

    output the second digital

---
+++ Task_011(task010)_outputTheSecocndDigital/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 10: Напишите программу, которая принимает 
+// на вход трёхзначное число и на выходе 
+// показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+Console.WriteLine("Введите целое трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+//int num = 583;//new Random().Next(90, 1000);
+
+int SecondDigital(int number)
+{
+    int firstTwo = num / 10;
+    int res = firstTwo % 10;
+    return res;
+}
+
+int result = SecondDigital(num);
+if (result == 0 || num < 100 || num > 999)
+    Console.WriteLine("Вы ввели недопустимое значение.");
+else
+    Console.WriteLine($"{num} --> {result}");
