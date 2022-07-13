bcc845bf1822a8eb8011717b8d3a14eeb8f8bf35
NazarovIlya <eliakohai@gmail.com>
2022-07-13 11:03:58 +0300

    added if operators, cjrrected the code

--- Task_013_outputTheThirdDigital/Program.cs
+++ Task_013_outputTheThirdDigital/Program.cs
@@ -11,11 +11,7 @@ int num = Convert.ToInt32(Console.ReadLine());
 int ThirdDigital(int number)
 {
     if (num / 100 == 0)
-    {
-        int res = 0;
-        res = num / 100;
-        return res;
-    }
+        return 0;
     else
     {
         int res = 0;
@@ -24,7 +20,9 @@ int ThirdDigital(int number)
     }
 }
 int result = ThirdDigital(num);
-if (result == 0)
-    Console.WriteLine("Третьей цифры нет");
+if (num >= 1000 || num < -999)
+    Console.WriteLine("Введеное число более трехзначного.");
+else if (result == 0)
+    Console.WriteLine("Третьей цифры нет. Возможно число не является трехзначным и находится в интервале от -99 до 99 или равно 100 / - 100");
 else
     Console.WriteLine($"{num} --> {result}");
