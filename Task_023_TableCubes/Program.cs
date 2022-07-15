c99b70b7d170838f599a5870c4894a0d079d4a5f
NazarovIlya <eliakohai@gmail.com>
2022-07-15 23:27:53 +0300

    table of cubes

---
+++ Task_023_TableCubes/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void TableCubes(int numMax)
+{
+    int num = 1;
+    while (num <= numMax)
+    {
+        Console.WriteLine($"{N} --> | {num} | {num * num * num}   |");
+        num++;
+    }
+}
+
+TableCubes(N);
