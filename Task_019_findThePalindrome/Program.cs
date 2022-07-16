c4a5c008567e368aee946d0c8b32a0dca0f03a63
NazarovIlya <eliakohai@gmail.com>
2022-07-16 17:37:16 +0300

    logic is divided: calculations are carried out in one method(IsPalindrome), output in another

--- Task_019_findThePalindrome/Program.cs
+++ Task_019_findThePalindrome/Program.cs
@@ -9,30 +9,33 @@
 // 100, 64540, 9000, 100000 - не являются палиндромами
 // Единственный баг - в отображении подобных чисел, нули впереверку опускаются
 
-void IsPalindrome(int N)
+int IsPalindrome(int n)
 {
-    int N2 = 0;
-    int NDiv = N;
+    int n2 = 0;
+    int nDiv = n;
     int lastDigital = 0;
 
-    if (N >= 0)
+    while (nDiv > 0)
     {
-        while (NDiv > 0)
-        {
-            lastDigital = NDiv % 10;
-            NDiv = NDiv / 10;
-            N2 = N2 * 10 + lastDigital;
-        }
+        lastDigital = nDiv % 10;
+        nDiv = nDiv / 10;
+        n2 = n2 * 10 + lastDigital;
     }
-    if (N == N2)
-        Console.WriteLine($"Число {N} является палиндромом, так если прочесть его с конца, получиться {N2}");
-    else if (N < 0)
-        Console.WriteLine($"Число {N} не является палиндромом, так если прочесть его с конца, получиться {N}-, знак минус после числа. Палиндром не математическое понятие.");
+    return n2;
+}
+
+void OutputResult(int number, int number2)
+{
+    if (number == number2)
+        Console.WriteLine($"Число {number} является палиндромом, так если прочесть его с конца, получится {number2}");
+    else if (number < 0)
+        Console.WriteLine($"Число {number} не является палиндромом, так если прочесть его с конца, получится {number * -1}-, знак минус после числа. Палиндром не математическое понятие.");
     else
-        Console.WriteLine($"Число {N} не является палиндромом, так если прочесть его с конца, получиться {N2}");
+        Console.WriteLine($"Число {number} не является палиндромом, так если прочесть его с конца, получится {number2}");
 }
 
 Console.Clear();
 Console.WriteLine("Введите целое число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-IsPalindrome(num);
+int num2 = IsPalindrome(num);
+
