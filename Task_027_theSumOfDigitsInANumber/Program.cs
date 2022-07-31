c63f10698e5d1745848b485592111a00af6eef5c
NazarovIlya <eliakohai@gmail.com>
2022-07-31 14:02:47 +0300

    the task is saved

--- Task_027_theSumOfDigitsInANumber/Program.cs
+++ Task_027_theSumOfDigitsInANumber/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 1
+
+Console.Clear();
 Console.WriteLine("Hello, World!");
