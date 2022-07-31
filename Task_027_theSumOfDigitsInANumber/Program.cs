8dd0894851f4addfc514ad47a70a53223fa9f975
NazarovIlya <eliakohai@gmail.com>
2022-07-31 14:35:07 +0300

    the task was completed in advance, 31/07/2022

--- Task_027_theSumOfDigitsInANumber/Program.cs
+++ Task_027_theSumOfDigitsInANumber/Program.cs
@@ -1,7 +1,36 @@
 ﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 // 452 -> 11
 // 82 -> 10
-// 9012 -> 1
+// 9012 -> 12
 
 Console.Clear();
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число, сумму цифр которого хотите узнать: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int SumOfDigits(int number)
+{
+    int digit = 0;
+    int sum = 0;
+    if (number > 0)
+    {
+        for (int i = 0; number > 0; i++)
+        {
+            digit = number % 10;
+            sum += digit;
+            number = number / 10;
+        }
+    }
+    else if (number < 0)
+    {
+        for (int i = 0; number < 0; i++)
+        {
+            digit = number % 10;
+            sum += digit;
+            number = number / 10;
+        }
+    }
+    return sum;
+}
+
+int result = SumOfDigits(num);
+Console.WriteLine($"Сумма цифр в числе {result}");
