fd8035a80b0d8a7de8e7dce3b71b3ceb097b9a38
NazarovIlya <eliakohai@gmail.com>
2022-07-31 15:47:06 +0300

    the task was completed in advance, 31/07/2022

--- Task_029_massiveOf8numbers/Program.cs
+++ Task_029_massiveOf8numbers/Program.cs
@@ -2,4 +2,26 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 
 Console.Clear();
-Console.WriteLine("Hello, World!");
+
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 1000);
+    }
+
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int[] arr = new int[8];
+FillArray(arr);
+Console.WriteLine("Задан массив из 8 чисел случайным образом:");
+PrintArray(arr);
