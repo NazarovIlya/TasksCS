0fc6cbdee35e0f9c08dc07cb6ccb131ba60649ad
NazarovIlya <eliakohai@gmail.com>
2022-08-02 22:44:36 +0300

    Composition 1 to N

---
+++ Task_028_composition1ToN/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int nA = Convert.ToInt32(Console.ReadLine());
+
+int Composition(int number)
+{
+    int composition = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        composition = composition * i;
+    }
+    return composition;
+}
+
+int result = Composition(nA);
+Console.WriteLine($"Произведение всех чисел от 1 до {nA} --> {result}");
