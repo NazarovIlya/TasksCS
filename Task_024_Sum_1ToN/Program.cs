c66d71b6f929925f3a1a5a0852e9f25e7877f084
NazarovIlya <eliakohai@gmail.com>
2022-08-02 22:46:28 +0300

    Sum all digits 1 to N

---
+++ Task_024_Sum_1ToN/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 24: Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int nA = Convert.ToInt32(Console.ReadLine());
+
+int Sum1ToN(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+//void PrintSum(int)
+
+int result = Sum1ToN(nA);
+Console.WriteLine($"Сумма {result}");
