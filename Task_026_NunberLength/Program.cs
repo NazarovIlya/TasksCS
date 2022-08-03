727027502aa7d63c3fd8d77024c040b1dae32e71
NazarovIlya <eliakohai@gmail.com>
2022-08-03 06:36:14 +0300

    count of digits, created print method

---
+++ Task_026_NunberLength/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Clear();
+Console.WriteLine("Ведите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int NumberLength(int number)
+{
+    int count = 0;
+    for (int i = 1; number > 0; i++)
+    {
+        number = number / 10;
+        count = i;
+    }
+    return count;
+}
+
+void PrintNumberLength(int number, int numberLength)
+{
+    Console.WriteLine($"Количество цифр в числе {number} --> {numberLength}");
+}
+
+int res = NumberLength(num);
+PrintNumberLength(num, res);
