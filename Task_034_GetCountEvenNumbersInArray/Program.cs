904be3b41ae15ef1b340a9506358fde152611315
NazarovIlya <eliakohai@gmail.com>
2022-08-06 02:40:47 +0300

    corrected mistake

--- Task_034_GetCountEvenNumbersInArray/Program.cs
+++ Task_034_GetCountEvenNumbersInArray/Program.cs
@@ -23,7 +23,7 @@ int GetCountEvenNumbers(int[] array)
     int evenCount = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i] % 2 == 0)
+        if (i % 2 == 0)
             evenCount++;
     }
     return evenCount;
