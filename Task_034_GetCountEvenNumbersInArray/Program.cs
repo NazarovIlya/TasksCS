897eb90390152679bcb331f1842bf565fc528a32
NazarovIlya <eliakohai@gmail.com>
2022-08-05 15:38:15 +0300

    the task is completed

--- Task_034_GetCountEvenNumbersInArray/Program.cs
+++ Task_034_GetCountEvenNumbersInArray/Program.cs
@@ -1,3 +1,55 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите количество трехзначных чисел: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] arr = FillArray(num);
+int res = GetCountEvenNumbers(arr);
+PrintResult(arr, res);
+
+int[] FillArray(int arrayLength)
+{
+    int[] array = new int[arrayLength];
+    Random rnd = new Random();
+    for (int i = 0; i < arrayLength; i++)
+    {
+        array[i] = rnd.Next(100, 1000);
+    }
+    return array;
+}
+
+int GetCountEvenNumbers(int[] array)
+{
+    int evenCount = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            evenCount++;
+    }
+    return evenCount;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+            Console.Write(", ");
+    }
+    Console.Write("] ");
+}
+
+void PrintResult(int[] array, int result)
+{
+    Console.Write("В массиве: ");
+    PrintArray(arr);
+    if (result == 1)
+        Console.Write($"находится {result} четное число");
+    else if (result > 1 && result < 5 || (result % 10) > 1 && (result % 10) > 5)
+        Console.Write($"находится {result} четных числа");
+    else Console.Write($"находится {result} четных чисел");
+}
+
+
