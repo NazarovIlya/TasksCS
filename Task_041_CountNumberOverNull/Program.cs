07cc80305072f38d98b4b35aee7f6fb066fd4cd5
NazarovIlya <eliakohai@gmail.com>
2022-08-06 17:20:21 +0300

    initial

---
+++ Task_041_CountNumberOverNull/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+int GetUserInputInt(string userInputStr)
+{
+    Console.WriteLine(userInputStr);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+
+
+
+
+Console.Clear();
+int numM = GetUserInputInt("Введите число: ");
