134c82cfe5e740e64f3cf3d0498ad3f477ab697d
NazarovIlya <eliakohai@gmail.com>
2022-08-09 16:58:24 +0300

    realised ConvertArray and GetCount method

--- Task_041_CountNumberOverNull/Program.cs
+++ Task_041_CountNumberOverNull/Program.cs
@@ -3,42 +3,54 @@
 // 0, 7, 8, -2, -2 -> 2
 // -1, -7, 567, 89, 223-> 3
 
-int[] GetUserInputInt(string userInputStr)
+string[] GetUserInputInt(string userInputStr)
 {
     Console.WriteLine(userInputStr);
-    string[] str = Console.ReadLine().Split(",");
-    int[] array = new int[str.Length];
-    for (int i = 0; i < str.Length; i++)
+    string[] strArray = Console.ReadLine().Split(",");
+    // int[] array = new int[str.Length];
+    // for (int i = 0; i < str.Length; i++)
+    // {
+    //     array[i] = Convert.ToInt32(str[i]);
+    // }
+    return strArray;
+}
+
+int[] ConvertArrayStrToInt(string[] strArray)
+{
+    int[] array = new int[strArray.Length];
+    for (int i = 0; i < strArray.Length; i++)
     {
-        array[i] = Convert.ToInt32(str[i]);
+        array[i] = Convert.ToInt32(strArray[i]);
     }
     return array;
 }
 
-// int GetCountNumbersOverNull(int[] array)
-// {
-//     int countNumbersOverNull = 0;
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         if (arrayp[i] > 0) countNumbersOverNull++;
-//     }
-//     return countNumbersOverNull;
-// }
-
-void PrintArray(int[] array, string outputText)
+int GetCountNumbersOverNull(int[] array)
 {
-    Console.WriteLine(outputText);
+    int countNumbersOverNull = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) countNumbersOverNull++;
+    }
+    return countNumbersOverNull;
+}
+
+void PrintArray(int[] array, string preOutputText, string postOutputText)
+{
+    Console.Write($"{preOutputText}");
     Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
         if (i < array.Length - 1)
             Console.Write($"{array[i]}, ");
-        else Console.WriteLine($"{array[i]}] ");
+        else Console.Write($"{array[i]}] ");
     }
+    Console.Write(postOutputText);
 }
 
 
-
-Console.Clear();
-int[] numM = GetUserInputInt("Введите число: ");
-PrintArray(numM, "");
+//Console.Clear();
+string[] stringNumM = GetUserInputInt("Введите числа, разделяя их запятой: ");
+int[] numM = ConvertArrayStrToInt(stringNumM);
+int countPosNumM = GetCountNumbersOverNull(numM);
+PrintArray(numM, "Получившейся массив: ", $"содержит {numM.Length} элементов, из которых {countPosNumM} положительных");
