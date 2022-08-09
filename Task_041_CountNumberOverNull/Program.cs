8122368bc6a7c8313a7c88d34d72274c23427de8
NazarovIlya <eliakohai@gmail.com>
2022-08-09 15:45:44 +0300

    realised input, print methods

--- Task_041_CountNumberOverNull/Program.cs
+++ Task_041_CountNumberOverNull/Program.cs
@@ -3,16 +3,42 @@
 // 0, 7, 8, -2, -2 -> 2
 // -1, -7, 567, 89, 223-> 3
 
-int GetUserInputInt(string userInputStr)
+int[] GetUserInputInt(string userInputStr)
 {
     Console.WriteLine(userInputStr);
-    int number = Convert.ToInt32(Console.ReadLine());
-    return number;
+    string[] str = Console.ReadLine().Split(",");
+    int[] array = new int[str.Length];
+    for (int i = 0; i < str.Length; i++)
+    {
+        array[i] = Convert.ToInt32(str[i]);
+    }
+    return array;
+}
+
+// int GetCountNumbersOverNull(int[] array)
+// {
+//     int countNumbersOverNull = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (arrayp[i] > 0) countNumbersOverNull++;
+//     }
+//     return countNumbersOverNull;
+// }
+
+void PrintArray(int[] array, string outputText)
+{
+    Console.WriteLine(outputText);
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+            Console.Write($"{array[i]}, ");
+        else Console.WriteLine($"{array[i]}] ");
+    }
 }
 
 
 
-
-
 Console.Clear();
-int numM = GetUserInputInt("Введите число: ");
+int[] numM = GetUserInputInt("Введите число: ");
+PrintArray(numM, "");
