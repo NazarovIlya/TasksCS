912ea0c5b9d1db77e5a08f8c7eb64017c8b6333e
NazarovIlya <eliakohai@gmail.com>
2022-08-09 23:37:46 +0300

    Binary

---
+++ Task_042_Binary/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+int UserInputInt(string userInputStr)
+{
+    Console.WriteLine(userInputStr);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void Binary(int number)
+{
+    //int count = 0;
+    if (number == 0) return;
+    Binary(number / 2);
+    Console.Write(number % 2);
+}
+
+Console.Clear();
+int num = UserInputInt("Enter");
+Binary(num);
