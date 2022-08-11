e879442dd8308924b05a518c1f604d83cebbf7ff
NazarovIlya <eliakohai@gmail.com>
2022-08-11 17:33:29 +0300

    the task is completed

--- Task_050_GetValueOfElementArray/Program.cs
+++ Task_050_GetValueOfElementArray/Program.cs
@@ -7,12 +7,71 @@
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
 
-int GetUserInputInt(string userInputString)
+int[] GetUserInputNubersInt(string userInputNumberString)
 {
-    Console.WriteLine(userInputString);
-    int number = Convert.ToInt32(Console.ReadLine());
-    return number;
+    Console.WriteLine(userInputNumberString);
+    string[] stringArray = Console.ReadLine().Split(",");
+    int[] numberArray = new int[stringArray.Length];
+    for (int i = 0; i < numberArray.Length; i++)
+    {
+        numberArray[i] = Convert.ToInt32(stringArray[i]);
+    }
+    return numberArray;
 }
 
+void FillMatrixTwoDimensionalInt(int[,] matrixTwoDimensional,
+                                int minIntervalValue, int maxIntervalValue)
+{
+    Random randomDouble = new Random();
+    for (int i = 0; i < matrixTwoDimensional.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixTwoDimensional.GetLength(1); j++)
+        {
+            matrixTwoDimensional[i, j] = randomDouble.Next(minIntervalValue, maxIntervalValue);
+        }
+    }
+}
 
+void PrintMatrixTwoDimensional(int[,] matrixTwoDimensional)
+{
+    for (int i = 0; i < matrixTwoDimensional.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixTwoDimensional.GetLength(1); j++)
+        {
+            if (matrixTwoDimensional[i, j] >= 0)
+                Console.Write($" {matrixTwoDimensional[i, j]} ");
+            else Console.Write($"{matrixTwoDimensional[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
 
+void PrintResult(int[] array, int[,] matrix)
+{
+    int i = 0;
+    int j = 1;
+    while (i < array.Length)
+    {
+        if (array[i + 1] < matrix.GetLength(0)
+            && array[i + 1] < matrix.GetLength(1))
+        {
+            Console.WriteLine($"Значение {j} интересующего Вас элемента: {matrix[array[i] - 1, array[i + 1] - 1]}");
+        }
+        else
+        {
+            Console.WriteLine($"Элемента в позиции {array[i]}, {array[i + 1]} нет в данном массиве.");
+        }
+        i = i + 2;
+        j++;
+    }
+}
+
+Console.Clear();
+// int nRows = GetUserInputInt("Введите количество строк массива: ");
+// int nColumns = GetUserInputInt("Введите количество столбцов массива: ");
+int[] RowColum = GetUserInputNubersInt("Введите количество строк и столбцов массива через запятую: ");
+int[,] matrix = new int[RowColum[0], RowColum[1]];
+int[] array = GetUserInputNubersInt("Введите номер строки и стобца интересующего Вас элемента матрицы через запятую: ");
+FillMatrixTwoDimensionalInt(matrix, -10, 10);
+PrintMatrixTwoDimensional(matrix);
+PrintResult(array, matrix);
