40c4e7136a5e5e3f698ff1fb92b12606d79a06d1
NazarovIlya <eliakohai@gmail.com>
2022-08-13 00:43:56 +0300

    MatrixSumElementMainDiagonal completed

---
+++ Task_051_MatrixSumElementMainDiagonal/Program.cs
@@ -0,0 +1,75 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму
+// элементов, находящихся на главной диагонали (с индексами
+// (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+int[] GetUserInputNumbersInt(string userInputNumberString)
+{
+    Console.WriteLine(userInputNumberString);
+    string[] stringArray = Console.ReadLine().Split(",");
+    int[] numberArray = new int[stringArray.Length];
+    for (int i = 0; i < numberArray.Length; i++)
+    {
+        numberArray[i] = Convert.ToInt32(stringArray[i]);
+    }
+    return numberArray;
+}
+
+int[,] FillMatrixTwoDimension(int[,] matrix, int min, int max)
+{
+    Random random = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = random.Next(min, max);
+        }
+    }
+    return matrix;
+}
+int GetMatrixSumElementMainDiagonal(int[,] matrixTwoDimensional)
+{
+    int sum = 0;
+    for (int i = 0; i < matrixTwoDimensional.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixTwoDimensional.GetLength(1); j++)
+        {
+            if (i == j)
+                sum += matrixTwoDimensional[i, j];
+        }
+    }
+    return sum;
+}
+
+void PrintMatrixTwoDimensional(int[,] matrixTwoDimensional, string userOutputString)
+{
+    Console.WriteLine(userOutputString);
+    for (int i = 0; i < matrixTwoDimensional.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < matrixTwoDimensional.GetLength(1); j++)
+        {
+            if (matrixTwoDimensional[i, j] >= 0)
+                Console.Write($" {matrixTwoDimensional[i, j]} ");
+            else Console.Write($"{matrixTwoDimensional[i, j]} ");
+        }
+        Console.Write(" ]");
+        Console.WriteLine();
+    }
+}
+
+
+Console.Clear();
+int[] matrixSize = GetUserInputNumbersInt("Введите количество строк и столбцов массива через запятую: ");
+int[] matrixMinMax = GetUserInputNumbersInt("Введите границы интервала случайных чисел: ");
+int[,] matrixProductRowColumn = new int[matrixSize[0], matrixSize[1]];
+FillMatrixTwoDimension(matrixProductRowColumn, matrixMinMax[0], matrixMinMax[1]);
+PrintMatrixTwoDimensional(matrixProductRowColumn, "Исходная матрица: ");
+int result = GetMatrixSumElementMainDiagonal(matrixProductRowColumn);
+Console.WriteLine($"{result}");
+
+
