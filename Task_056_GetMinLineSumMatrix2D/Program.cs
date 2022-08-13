6d887a780ea3032b1de132f061223164ce5937cd
NazarovIlya <eliakohai@gmail.com>
2022-08-13 19:29:10 +0300

    initial

---
+++ Task_056_GetMinLineSumMatrix2D/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[] GetUserInputNumbersInt(string userInputNumberString)
+{
+    Console.WriteLine(userInputNumberString);
+    string[] stringArray = Console.ReadLine().Split(",");
+    int[] numberArray = new int[stringArray.Length];
+    for (int i = 0; i < numberArray.Length; i++)
+    {
+        if (stringArray[i] == "" || stringArray[i] == " "
+            || Convert.ToInt32(stringArray[i]) == null)
+        {
+            Console.WriteLine("Ошибка ввода данных. Попробуйте еще раз запустить программу и ввести данные корректно.");
+            Environment.Exit(0);
+        }
+        else numberArray[i] = Convert.ToInt32(stringArray[i]);
+    }
+    return numberArray;
+}
+
+void FillMatrix2DInt(int[,] matrix, int min, int max)
+{
+    Random random = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = random.Next(min, max);
+        }
+    }
+}
+
+void GetMinSumLineMatrix2D(int[,] matrix2D)
+{
+    for (int i = 0; i < matrix2D.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix2D.GetLength(1); j++)
+        {
+
+        }
+    }
+}
+
+void PrintMatrix2DInt(int[,] matrixTwoDimensional, string userOutputString)
+{
+    Console.WriteLine(userOutputString);
+    for (int i = 0; i < matrixTwoDimensional.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < matrixTwoDimensional.GetLength(1); j++)
+        {
+            if (matrixTwoDimensional[i, j] >= 0)
+                Console.Write($" {matrixTwoDimensional[i, j]} ");
+            else Console.Write($"{matrixTwoDimensional[i, j]} ");
+        }
+        Console.Write(" ]");
+        Console.WriteLine();
+    }
+}
+
+
+//Console.Clear();
