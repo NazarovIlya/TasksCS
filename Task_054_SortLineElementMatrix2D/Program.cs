de56912a818b690717f26d763aa967d885ca0173
NazarovIlya <eliakohai@gmail.com>
2022-08-13 19:05:26 +0300

    updated

--- Task_054_SortLineElementMatrix2D/Program.cs
+++ Task_054_SortLineElementMatrix2D/Program.cs
@@ -78,7 +78,7 @@ void PrintMatrix2DInt(int[,] matrixTwoDimensional, string userOutputString)
 }
 
 
-//Console.Clear();
+Console.Clear();
 int[] matrixSize = GetUserInputNumbersInt("Введите размер матрицы (n строк,n столбцов): ");
 int[] matrixMinMax = GetUserInputNumbersInt("Введите границы интервала случайных чисел (через запятую): ");
 int[,] matrixToSort = new int[matrixSize[0], matrixSize[1]];
