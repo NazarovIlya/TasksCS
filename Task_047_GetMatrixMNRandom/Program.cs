960da82e391c6efb90f55d1c3dbcc9da4abe1852
NazarovIlya <eliakohai@gmail.com>
2022-08-13 13:45:06 +0300

    added input error check into GetUserInputNumbersInt method

--- Task_047_GetMatrixMNRandom/Program.cs
+++ Task_047_GetMatrixMNRandom/Program.cs
@@ -19,7 +19,13 @@ int[] GetUserInputNumbersInt(string userInputNumberString)
     int[] numberArray = new int[stringArray.Length];
     for (int i = 0; i < numberArray.Length; i++)
     {
-        numberArray[i] = Convert.ToInt32(stringArray[i]);
+        if (stringArray[i] == "" || stringArray[i] == " "
+           || Convert.ToInt32(stringArray[i]) == null)
+        {
+            Console.WriteLine("Ошибка ввода данных. Попробуйте еще раз запустить программу и ввести данные корректно.");
+            Environment.Exit(0);
+        }
+        else numberArray[i] = Convert.ToInt32(stringArray[i]);
     }
     return numberArray;
 }
