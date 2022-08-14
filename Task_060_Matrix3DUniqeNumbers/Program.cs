b38bdfce47233b6f0c43e417c6dc882eb4ca25d2
NazarovIlya <eliakohai@gmail.com>
2022-08-14 17:39:48 +0300

    added FillMatrix3DInt, PrintMatrix3DInt methods

--- Task_060_Matrix3DUniqeNumbers/Program.cs
+++ Task_060_Matrix3DUniqeNumbers/Program.cs
@@ -6,42 +6,80 @@
 // 12(0,0,0) 22(0,0,1)
 // 45(1,0,0) 53(1,0,1)
 
-int[] GetUserInputNumbersInt(string userInputNumberString)
+string[] GetUserInputNumbersString(string userInputTextString)
 {
-    Console.WriteLine(userInputNumberString);
-    string[] stringArray = Console.ReadLine().Split(",");
-    int[] numberArray = new int[stringArray.Length];
-    for (int i = 0; i < numberArray.Length; i++)
+    Console.WriteLine(userInputTextString);
+    string[] userInputString = Console.ReadLine().Split(",");
+    return userInputString;
+}
+
+string[] CheckUserInputToInt(string[] userInputString)
+{
+    for (int i = 0; i < userInputString.Length; i++)
     {
-        if (stringArray[i] == "" || stringArray[i] == " "
-            || Convert.ToInt32(stringArray[i]) == null)
+        if (userInputString[i] == string.Empty || userInputString[i] == " "
+            || Convert.ToInt32(userInputString[i]) == null)
         {
             Console.WriteLine("Ошибка ввода данных. Попробуйте еще раз запустить программу и ввести данные корректно.");
             Environment.Exit(0);
         }
-        else numberArray[i] = Convert.ToInt32(stringArray[i]);
     }
-    return numberArray;
+    return userInputString;
 }
 
-void FillMatrix3DInt(int[,] matrix3D, int min, int max)
+int[] ConvertUserInputNumbersInt(string[] userNumberString)
+{
+    int[] userNumberInt = new int[userNumberString.Length];
+    for (int i = 0; i < userNumberString.Length; i++)
+    {
+        userNumberInt[i] = Convert.ToInt32(userNumberString[i]);
+    }
+    return userNumberInt;
+}
+
+void FillMatrix3DInt(int[,,] matrix3D, int min, int max)
 {
     Random random = new Random();
     for (int i = 0; i < matrix3D.GetLength(0); i++)
     {
         for (int j = 0; j < matrix3D.GetLength(1); j++)
         {
-            for (int k = 0; k < matrix3D; k++)
+            for (int k = 0; k < matrix3D.GetLength(2); k++)
             {
-                int randomNumber = random.Next(min, max);
-                //if (matrix3D.[i, j, k] = matrix3D[i, j, k - 1])
-                if (matrix3D.IndexOf)
+                // matrix3D[i, j, k] = random.Next(min, max);
             }
         }
     }
 }
 
+void PrintMatrix3DInt(int[,,] matrix3D, string userOutputString)
+{
+    Console.WriteLine(userOutputString);
+    for (int i = 0; i < matrix3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix3D.GetLength(1); j++)
+        {
+            Console.Write("[");
+            for (int k = 0; k < matrix3D.GetLength(2); k++)
+            {
+                if (matrix3D[i, j, k] >= 0)
+                    Console.Write($" {matrix3D[i, j, k]} ({i}, {j}, {k}) ");
+                else Console.Write($"{matrix3D[i, j, k]} ({i}, {j}, {k}) ");
+            }
+            Console.Write("]");
+            Console.WriteLine();
+        }
+    }
+}
 
 
 //Console.Clear();
-int matrixSize = GetUserInputNumbersInt("");
+string[] matrixSizeString = GetUserInputNumbersString("Введите размер массива (три значения через запятую): ");
+matrixSizeString = CheckUserInputToInt(matrixSizeString);
+int[] matrixSizeInt = ConvertUserInputNumbersInt(matrixSizeString);
+string[] intervalMinMaxString = GetUserInputNumbersString("Введите границы интервала случайных чисел: ");
+intervalMinMaxString = CheckUserInputToInt(intervalMinMaxString);
+int[] intervalMinMaxInt = ConvertUserInputNumbersInt(intervalMinMaxString);
+int[,,] matrix3D = new int[matrixSizeInt[0], matrixSizeInt[1], matrixSizeInt[2]];
+FillMatrix3DInt(matrix3D, intervalMinMaxInt[0], intervalMinMaxInt[1]);
+PrintMatrix3DInt(matrix3D, "Полученный массив: ");
--- Task_062_SpiralFillMatrix2D/Program.cs
+++ Task_062_SpiralFillMatrix2D/Program.cs
@@ -5,6 +5,36 @@
 // 11 16 15 6
 // 10 9 8 7
 
+string[] GetUserInputNumbersString(string userInputTextString)
+{
+    Console.WriteLine(userInputTextString);
+    string[] userInputString = Console.ReadLine().Split(",");
+    return userInputString;
+}
+
+string[] CheckUserInputToInt(string[] userInputString)
+{
+    for (int i = 0; i < userInputString.Length; i++)
+    {
+        if (userInputString[i] == string.Empty || userInputString[i] == " "
+            || Convert.ToInt32(userInputString[i]) == null)
+        {
+            Console.WriteLine("Ошибка ввода данных. Попробуйте еще раз запустить программу и ввести данные корректно.");
+            Environment.Exit(0);
+        }
+    }
+    return userInputString;
+}
+
+int[] ConvertUserInputNumbersInt(string[] userNumberString)
+{
+    int[] userNumberInt = new int[userNumberString.Length];
+    for (int i = 0; i < userNumberString.Length; i++)
+    {
+        userNumberInt[i] = Convert.ToInt32(userNumberString[i]);
+    }
+    return userNumberInt;
+}
 
 
 
