e65a827e6f51d2a7f3d6e99a961ada6da79b177d
NazarovIlya <eliakohai@gmail.com>
2022-08-14 13:11:36 +0300

    updated GetUserInputNumberString

--- Task_060_Matrix3DUniqeNumbers/Program.cs
+++ Task_060_Matrix3DUniqeNumbers/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, 
+// добавляя индексы каждого элемента.
+
+// массив размером 2 x 2 x 2
+// 12(0,0,0) 22(0,0,1)
+// 45(1,0,0) 53(1,0,1)
+
+int[] GetUserInputNumbersInt(string userInputNumberString)
+{
+    Console.WriteLine(userInputNumberString);
+    string[] stringArray = Console.ReadLine().Split(",");
+    int[] numberArray = new int[stringArray.Length];
+    for (int i = 0; i < numberArray.Length; i++)
+    {
+        if (stringArray[i] == "" || stringArray[i] == " "
+            || Convert.ToInt32(stringArray[i]) == null)
+        {
+            Console.WriteLine("Ошибка ввода данных. Попробуйте еще раз запустить программу и ввести данные корректно.");
+            Environment.Exit(0);
+        }
+        else numberArray[i] = Convert.ToInt32(stringArray[i]);
+    }
+    return numberArray;
+}
+
+void FillMatrix3DInt(int[,] matrix3D, int min, int max)
+{
+    Random random = new Random();
+    for (int i = 0; i < matrix3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix3D.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix3D; k++)
+            {
+                int randomNumber = random.Next(min, max);
+                //if (matrix3D.[i, j, k] = matrix3D[i, j, k - 1])
+                if (matrix3D.IndexOf)
+            }
+        }
+    }
+}
+
+
+
+//Console.Clear();
+int matrixSize = GetUserInputNumbersInt("");
