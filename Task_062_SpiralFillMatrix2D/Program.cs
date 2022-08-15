7c7d55d43d7f1480c5e4e170c90d7ad2abe5634d
NazarovIlya <eliakohai@gmail.com>
2022-08-15 13:41:33 +0300

    PrintMatrix2DInt

--- Task_062_SpiralFillMatrix2D/Program.cs
+++ Task_062_SpiralFillMatrix2D/Program.cs
@@ -50,6 +50,23 @@ void FillSpiralMatrix2DInt(int[,,] matrix2D,
     }
 }
 
+void PrintMatrix2DInt(int[,] matrixTwoDimensional, string userOutputString)
+{
+    Console.WriteLine(userOutputString);
+    for (int i = 0; i < matrixTwoDimensional.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < matrixTwoDimensional.GetLength(1); j++)
+        {
+            if (matrixTwoDimensional[i, j] >= 0)
+                Console.Write($" {matrixTwoDimensional[i, j]} ");
+            else Console.Write($"{matrixTwoDimensional[i, j]} ");
+        }
+        Console.Write(" ]");
+        Console.WriteLine();
+    }
+}
+
 
 
 
