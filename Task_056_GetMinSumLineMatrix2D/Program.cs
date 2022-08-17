403a90dae0159977d95d626080aa5fee3d8c07e4
NazarovIlya <eliakohai@gmail.com>
2022-08-17 10:45:49 +0300

    the user input method is divided into 3 different methods

--- Task_056_GetMinSumLineMatrix2D/Program.cs
+++ Task_056_GetMinSumLineMatrix2D/Program.cs
@@ -7,22 +7,34 @@
 // 5 2 6 7
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
-int[] GetUserInputNumbersInt(string userInputNumberString)
+string[] GetUserInputNumbersString(string userInputTextString)
 {
-    Console.WriteLine(userInputNumberString);
-    string[] stringArray = Console.ReadLine().Split(",");
-    int[] numberArray = new int[stringArray.Length];
-    for (int i = 0; i < numberArray.Length; i++)
+    Console.WriteLine(userInputTextString);
+    string[] userInputString = Console.ReadLine().Split(",");
+    return userInputString;
+}
+
+void CheckUserInputToInt(string[] userInputString)
+{
+    for (int i = 0; i < userInputString.Length; i++)
     {
-        if (stringArray[i] == string.Empty || stringArray[i] == " "
-            || Convert.ToInt32(stringArray[i]) == null)
+        if (userInputString[i] == string.Empty || userInputString[i] == " "
+            || Convert.ToInt32(userInputString[i]) == null)
         {
             Console.WriteLine("Ошибка ввода данных. Попробуйте еще раз запустить программу и ввести данные корректно.");
             Environment.Exit(0);
         }
-        else numberArray[i] = Convert.ToInt32(stringArray[i]);
     }
-    return numberArray;
+}
+
+int[] ConvertUserInputNumbersInt(string[] userNumberString)
+{
+    int[] userNumberInt = new int[userNumberString.Length];
+    for (int i = 0; i < userNumberString.Length; i++)
+    {
+        userNumberInt[i] = Convert.ToInt32(userNumberString[i]);
+    }
+    return userNumberInt;
 }
 
 void FillMatrix2DInt(int[,] matrix, int min, int max)
@@ -76,10 +88,14 @@ void PrintMatrix2DInt(int[,] matrixTwoDimensional, string userOutputString)
 
 
 Console.Clear();
-int[] matrixSize = GetUserInputNumbersInt("Введите размер матрицы (n строк,n столбцов): ");
-int[] matrixMinMax = GetUserInputNumbersInt("Введите границы интервала случайных чисел (через запятую): ");
-int[,] matrix = new int[matrixSize[0], matrixSize[1]];
-FillMatrix2DInt(matrix, matrixMinMax[0], matrixMinMax[1]);
+string[] matrixSizeString = GetUserInputNumbersString("Введите размер матрицы (n строк,n столбцов): ");
+CheckUserInputToInt(matrixSizeString);
+int[] matrixSizeInt = ConvertUserInputNumbersInt(matrixSizeString);
+string[] matrixMinMaxString = GetUserInputNumbersString("Введите границы интервала случайных чисел (через запятую): ");
+CheckUserInputToInt(matrixMinMaxString);
+int[] matrixMinMaxInt = ConvertUserInputNumbersInt(matrixMinMaxString);
+int[,] matrix = new int[matrixSizeInt[0], matrixSizeInt[1]];
+FillMatrix2DInt(matrix, matrixMinMaxInt[0], matrixMinMaxInt[1]);
 PrintMatrix2DInt(matrix, "Задан массив: ");
 int result = GetMinSumLineMatrix2D(matrix);
 Console.WriteLine($"Наименьшая сумма значений всех элементов в {result} строке (машинное исчисление от нуля).");
