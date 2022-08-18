1f9dcf40c35a2206c54c4b70c8eeea7753ab31a2
NazarovIlya <eliakohai@gmail.com>
2022-08-18 13:11:19 +0300

    FillMatrix2DInt method added

--- Task_059_RemoveRowColumnWhithMinNumber/Program.cs
+++ Task_059_RemoveRowColumnWhithMinNumber/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 59: Задайтедвумерный массив из целых чисел.
+// Напишите программу, которая удалит строку и столбец, на
+// пересечении которых расположен наименьший элемент
+// массива.
+
+
+
+void FillMatrix2DInt(int[,] matrixTwoDimensional,
+                                int minIntervalValue, int maxIntervalValue)
+{
+    Random random = new Random();
+    for (int i = 0; i < matrixTwoDimensional.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixTwoDimensional.GetLength(1); j++)
+        {
+            matrixTwoDimensional[i, j] = random.Next(minIntervalValue, maxIntervalValue);
+        }
+    }
+}
