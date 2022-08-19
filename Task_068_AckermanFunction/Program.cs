3d915ed5ad5069fbc7e9db69f4310316ca4d68d6
NazarovIlya <eliakohai@gmail.com>
2022-08-19 14:54:49 +0300

    method completed, have to update program

--- Task_068_AckermanFunction/Program.cs
+++ Task_068_AckermanFunction/Program.cs
@@ -1,3 +1,18 @@
 ﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
 // Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 29
+
+int GetAckermanFunction(int argM, int argN)
+{
+    if (argM < 0 && argN < 0)
+        return -1;    // Функция Аккермана имеет решение только для положительных значений аргументов");
+    if (argM == 0)
+        return argN + 1;
+    if (argM > 0 && argN == 0)
+        return GetAckermanFunction(argM - 1, 1);
+    //if (argM > 0 && argN > 0) 
+    return GetAckermanFunction(argM - 1, GetAckermanFunction(argM, argN - 1));
+}
+
+int result = GetAckermanFunction(4, 4);
+Console.WriteLine($"{result}");
