ed371195ae4a9c83885ad690cfb0e9b03821840c
NazarovIlya <eliakohai@gmail.com>
2022-08-19 23:42:25 +0300

    the task completed

---
+++ Task_067_SumNumberDigitsRecursive/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int GetUserInputInt(string userInputString)
+{
+    Console.WriteLine(userInputString);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int GetSumDigits(int number, int sum)
+{
+    if (number == 0) return sum;
+    sum += number % 10;
+    return GetSumDigits(number / 10, sum);
+}
+
+
+//Console.Clear();
+int num = GetUserInputInt("Введите число больше 9: ");
+int result = GetSumDigits(num, 0);
+Console.WriteLine($"Сумма всех цифр в числе: {num} --> {result}.");
