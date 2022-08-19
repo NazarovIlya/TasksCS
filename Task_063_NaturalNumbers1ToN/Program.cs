b99dc2b3fe2b01f834e311ce4d0b6e7099a915dd
NazarovIlya <eliakohai@gmail.com>
2022-08-19 23:17:53 +0300

    completed

---
+++ Task_063_NaturalNumbers1ToN/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+int GetUserInputInt(string userInputString)
+{
+    Console.WriteLine(userInputString);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void NaturalNumber(int number)
+{
+    if (number == 0) return;
+    NaturalNumber(number - 1);
+    Console.Write($"{number} ");
+}
+
+int num = GetUserInputInt("Введите натуральное число: ");
+Console.WriteLine("Натуральные числа от 1 до N: ");
+NaturalNumber(num);
