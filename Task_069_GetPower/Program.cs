3ad3d94ee30befed72a20301abdd484ef344445e
NazarovIlya <eliakohai@gmail.com>
2022-08-20 00:04:00 +0300

    the task completed

---
+++ Task_069_GetPower/Program.cs
@@ -0,0 +1,49 @@
+﻿/// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+string[] GetUserInputNumbersString(string userInputTextString)
+{
+    Console.WriteLine(userInputTextString);
+    string[] userInputString = Console.ReadLine().Split(",");
+    return userInputString;
+}
+
+void CheckUserInputToInt(string[] userInputString)
+{
+    for (int i = 0; i < userInputString.Length; i++)
+    {
+        if (userInputString[i] == string.Empty || userInputString[i] == " "
+            || Convert.ToInt32(userInputString[i]) == null
+            || userInputString.Length < 2)
+        {
+            Console.WriteLine("Ошибка ввода данных. Попробуйте еще раз запустить программу и ввести данные корректно.");
+            Environment.Exit(0);
+        }
+    }
+}
+
+int[] ConvertUserInputNumbersInt(string[] userNumberString)
+{
+    int[] userNumberInt = new int[userNumberString.Length];
+    for (int i = 0; i < userNumberString.Length; i++)
+    {
+        userNumberInt[i] = Convert.ToInt32(userNumberString[i]);
+    }
+    return userNumberInt;
+}
+
+int GetPower(int a, int b)
+{
+    if (b == 0 && b == 1) return a;
+    if (b > 1) return a * GetPower(a, b - 1);
+    return a;
+}
+
+//Console.Clear();
+string[] arrayAB = GetUserInputNumbersString("Введите натуральное число A и значение натуральной степни B:");
+CheckUserInputToInt(arrayAB);
+int[] arrayABInt = ConvertUserInputNumbersInt(arrayAB);
+int result = GetPower(arrayABInt[0], arrayABInt[1]);
+Console.WriteLine($"{result}");
