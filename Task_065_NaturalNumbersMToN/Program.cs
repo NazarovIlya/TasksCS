4e766388342587a95324736eaca7a4550bb42837
NazarovIlya <eliakohai@gmail.com>
2022-08-19 23:23:25 +0300

    the task was partially completed

---
+++ Task_065_NaturalNumbersMToN/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+
+string[] GetUserInputNumbersString(string userInputTextString)
+{
+    Console.WriteLine(userInputTextString);
+    string[] userInputString = Console.ReadLine().Split(",");
+    return userInputString;
+}
+
+void CheckUserInputToInt(string[] userInputString)
+{
+    for (int i = 0; i < userInputString.Length; i++)
+    {
+        if (userInputString[i] == string.Empty || userInputString[i] == " "
+            || Convert.ToInt32(userInputString[i]) == null
+            || userInputString.Length < 2)
+        {
+            Console.WriteLine("Ошибка ввода данных. Попробуйте еще раз запустить программу и ввести данные корректно.");
+            Environment.Exit(0);
+        }
+    }
+}
+
+int[] ConvertUserInputNumbersInt(string[] userNumberString)
+{
+    int[] userNumberInt = new int[userNumberString.Length];
+    for (int i = 0; i < userNumberString.Length; i++)
+    {
+        userNumberInt[i] = Convert.ToInt32(userNumberString[i]);
+    }
+    return userNumberInt;
+}
+
+void WriteNaturalNumbersNToM(int m, int n)
+{
+    if (m == n)
+    {
+        Console.Write($"{m} ");
+        return;
+    }
+    else if (m > n) return;
+    {
+        Console.Write($"{m} ");
+        WriteNaturalNumbersNToM(++m, n);
+    }
+    // if (n > m) return;
+    // {
+    //     Console.Write($"{m} ");
+    //     WriteNaturalNumbersNToM(--m, n);
+    // }
+}
+
+
+Console.Clear();
+string[] arrayNaturalNumberIntervalString = GetUserInputNumbersString("Введите интервал значений натуральных чисел: ");
+CheckUserInputToInt(arrayNaturalNumberIntervalString);
+int[] arrayNaturalNumberIntervalInt = ConvertUserInputNumbersInt(arrayNaturalNumberIntervalString);
+Console.WriteLine("В заданном интервале натуральными являются: ");
+WriteNaturalNumbersNToM(arrayNaturalNumberIntervalInt[0], arrayNaturalNumberIntervalInt[1]);
