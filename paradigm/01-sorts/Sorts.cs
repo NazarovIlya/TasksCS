9da2d7ee522cad34d38fab6074a0bc629b8bf943
NazarovIlya <eliakohai@gmail.com>
2023-09-18 19:18:48 +0300

    updated

--- paradigm/01-sorts/Sorts.cs
+++ paradigm/01-sorts/Sorts.cs
@@ -6,55 +6,55 @@ using System.Threading.Tasks;
 
 namespace _01_sorts
 {
-	internal class Sorts
-	{
-		public Sorts() { }
+  internal class Sorts
+  {
+    public Sorts() { }
 
-		public int[] SortImperative(int[] inputArray)
-		{
-			return QuickSort(inputArray, 0, inputArray.Length - 1);
-		}
+    public int[] SortImperative(int[] inputArray)
+    {
+      return QuickSort(inputArray, 0, inputArray.Length - 1);
+    }
 
-		public int[] SortDeclarative(int[] inputArray)
-		{
-			Array.Sort(inputArray);
-			return inputArray;
-		}
-
-
-
-		private int[] QuickSort(int[] array, int minIndex, int maxIndex)
-		{
-			if (minIndex >= maxIndex) return array;
-			int pivot = GetPivotIndex(array, minIndex, maxIndex);
-			QuickSort(array, minIndex, pivot - 1);
-			QuickSort(array, pivot + 1, maxIndex);
-			return array;
-		}
+    public int[] SortDeclarative(int[] inputArray)
+    {
+      Array.Sort(inputArray);
+      return inputArray;
+    }
 
 
-		private int GetPivotIndex(int[] array, int minIndex, int maxIndex)
-		{
-			int pivotIndex = minIndex - 1;
-			for (int i = minIndex; i <= maxIndex; i++)
-			{
-				if (array[i] < array[maxIndex])
-				{
-					pivotIndex++;
-					Swap(array, i, pivotIndex);
-				}
-			}
-			pivotIndex++;
-			Swap(array, pivotIndex, maxIndex);
-			return pivotIndex;
-		}
+
+    private int[] QuickSort(int[] array, int minIndex, int maxIndex)
+    {
+      if (minIndex >= maxIndex) return array;
+      int pivot = GetPivotIndex(array, minIndex, maxIndex);
+      QuickSort(array, minIndex, pivot - 1);
+      QuickSort(array, pivot + 1, maxIndex);
+      return array;
+    }
 
 
-		private void Swap(int[] array, int leftValue, int rightValue)
-		{
-			int temp = array[leftValue];
-			array[leftValue] = array[rightValue];
-			array[rightValue] = temp;
-		}
-	}
+    private int GetPivotIndex(int[] array, int minIndex, int maxIndex)
+    {
+      int pivotIndex = minIndex - 1;
+      for (int i = minIndex; i <= maxIndex; i++)
+      {
+        if (array[i] < array[maxIndex])
+        {
+          pivotIndex++;
+          Swap(array, i, pivotIndex);
+        }
+      }
+      pivotIndex++;
+      Swap(array, pivotIndex, maxIndex);
+      return pivotIndex;
+    }
+
+
+    private void Swap(int[] array, int leftValue, int rightValue)
+    {
+      int temp = array[leftValue];
+      array[leftValue] = array[rightValue];
+      array[rightValue] = temp;
+    }
+  }
 }
