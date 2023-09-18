cb6585a4b4b0e8516ec722b53d773d21a2be6cfe
NazarovIlya <eliakohai@gmail.com>
2023-09-18 19:02:31 +0300

    updated

--- paradigm/02-Sort/02-Sort/Sort.cs
+++ paradigm/02-Sort/02-Sort/Sort.cs
@@ -1,31 +1,24 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _02_Sort
+﻿namespace _02_Sort
 {
-	internal class Sort
-	{
-		public Sort() { }
+  internal class Sort
+  {
+    public Sort() { }
 
-		public int[] BubbleSort(int[] array) 
-		{ 
-			for (int i = 0; i < array.Length; i++)
-			{
-				for (int j = 0; j < array.Length - i - 1; j++)
-				{
-					if (array[j] > array[j + 1])
-					{
-						int temp = array[j];
-						array[j] = array[j + 1];
-						array[j + 1] = temp;
-					}
-				}
-			}
-			return array;
-		}
-	}
+    public int[] BubbleSort(int[] array)
+    {
+      for (int i = 0; i < array.Length; i++)
+      {
+        for (int j = 0; j < array.Length - i - 1; j++)
+        {
+          if (array[j] > array[j + 1])
+          {
+            int temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+          }
+        }
+      }
+      return array;
+    }
+  }
 }
